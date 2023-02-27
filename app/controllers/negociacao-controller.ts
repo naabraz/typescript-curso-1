f777eb38af72d7867c8e81abbc7e9b3c3f76a478
Natalia Braz <naabraz@gmail.com>
2023-02-27 09:07:32 -0300

    chore: add date validation

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -9,7 +9,9 @@ export class NegociacaoController {
     private inputValor: HTMLInputElement;
     private negociacoes = new Negociacoes();
     private negociacoesView = new NegociacoesView('#negociacoesView');
-    private mensagemView = new MensagemView('#mensagemView')
+    private mensagemView = new MensagemView('#mensagemView');
+    private readonly SABADO = 6;
+    private readonly DOMINGO = 0;
 
     constructor() {
         this.inputData = document.querySelector('#data');
@@ -20,9 +22,20 @@ export class NegociacaoController {
 
     public adiciona(): void {
         const negociacao = this.criaNegociacao();
+
+        if (!this.ehDiaUtil(negociacao.data)) {
+         this.mensagemView
+                .update('Apenas negociações em dias úteis são aceitas');
+            return;
+        }
+
         this.negociacoes.adiciona(negociacao);
         this.limparFormulario();
         this.atualizaView();
+    }
+
+    private ehDiaUtil(data: Date) {
+        return data.getDay() > this.DOMINGO && data.getDay() < this.SABADO
     }
 
     private criaNegociacao(): Negociacao {
