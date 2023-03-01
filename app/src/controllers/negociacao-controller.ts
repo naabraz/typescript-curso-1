dba0ef21ca6f54b501597e2ec14f2652c840a173
Natalia Braz <naabraz@gmail.com>
2023-03-01 09:49:30 -0300

    chore: add decorator

--- app/src/controllers/negociacao-controller.ts
+++ app/src/controllers/negociacao-controller.ts
@@ -3,6 +3,7 @@ import { Negociacoes } from '../models/negociacoes.js';
 import { MensagemView } from '../views/mensagem-view.js';
 import { NegociacoesView } from '../views/negociacoes-view.js';
 import { DiaDaSemana } from '../enums/dias-da-semana.js';
+import { logarTempoDeExecucao } from '../decorators/logar-tempo-de-execucao.js';
 
 export class NegociacaoController {
     private inputData: HTMLInputElement;
@@ -19,13 +20,13 @@ export class NegociacaoController {
         this.negociacoesView.update(this.negociacoes);
     }
 
+    @logarTempoDeExecucao()
     public adiciona(): void {
         const negociacao = Negociacao.criaDe(
             this.inputData.value,
             this.inputQuantidade.value,
             this.inputValor.value
         );
-
 
         if (!this.ehDiaUtil(negociacao.data)) {
          this.mensagemView
