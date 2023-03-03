d9e9d3cb418a0ee13e7feba6d52a1e3fed763bbd
Natalia Braz <naabraz@gmail.com>
2023-03-03 09:43:28 -0300

    chore: add cache to decorator

--- app/dist/js/decorators/dom-injector.js
+++ app/dist/js/decorators/dom-injector.js
@@ -2,10 +2,13 @@ export function domInjector(seletor) {
     return function (target, propertyKey) {
         console.log(`Modificando prototype ${target.constructor.name}
       e adicionando getter para a propriedade ${propertyKey}`);
+        let elemento;
         const getter = function () {
-            const elemento = document.querySelector(seletor);
-            console.log(`buscando elemento do DOM com o
+            if (!elemento) {
+                elemento = document.querySelector(seletor);
+                console.log(`buscando elemento do DOM com o
         ${seletor} para injetar em ${propertyKey}`);
+            }
             return elemento;
         };
         Object.defineProperty(target, propertyKey, {
--- app/src/decorators/dom-injector.ts
+++ app/src/decorators/dom-injector.ts
@@ -4,11 +4,14 @@ export function domInjector(seletor: string) {
     console.log(`Modificando prototype ${target.constructor.name}
       e adicionando getter para a propriedade ${propertyKey}`);
 
-    const getter = function() {
-      const elemento = document.querySelector(seletor);
+    let elemento: HTMLElement;
 
-      console.log(`buscando elemento do DOM com o
+    const getter = function() {
+      if (!elemento) {
+        elemento = <HTMLElement>document.querySelector(seletor);
+        console.log(`buscando elemento do DOM com o
         ${seletor} para injetar em ${propertyKey}`);
+      }
 
       return elemento;
     }
